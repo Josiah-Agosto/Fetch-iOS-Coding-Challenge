d65896dd2c7f306d9ec3de1379cb68de12477503
Josiah Agosto <agosto_josiah@yahoo.com>
2024-06-21 17:01:05 -0400

    Initial Commit

---
+++ Fetch iOS Coding Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fetch iOS Coding Challenge
+//
+//  Created by Josiah Agosto on 6/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
